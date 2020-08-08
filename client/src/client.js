78bec4f2d4295116fd73bfc94e6a1c32b935d649
azevedorafael <you@example.com>
2020-08-07 23:45:20 -0300

    add optmistic ui

--- client/src/client.js
+++ client/src/client.js
@@ -1,13 +1,21 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
-
-/**
- * Create an Apollo Client, Link it to an API,
- *  and export it as a default client
- */
-
-const link = new HttpLink({ uri: "http://localhost:4000/" });
+import { setContext } from 'apollo-link-context'
+import { ApolloLink } from 'apollo-link'
+const http = new HttpLink({ uri: "http://localhost:4000/" });
+const delay = setContext(
+  request => 
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success()
+      }, 800)
+    })
+)
+const link = ApolloLink.from([
+  delay,
+  http
+])
 const cache = new InMemoryCache()
 const client = new ApolloClient({
   link,
